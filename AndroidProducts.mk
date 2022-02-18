83cd85b0c7c3e9907ecf676a82dcf115267ccf63
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-02-19 02:09:23 +0530

    lavender: Nusantara-Ify
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_lavender.mk
+    $(LOCAL_DIR)/nad_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_lavender-eng \
-    revengeos_lavender-userdebug \
-    revengeos_lavender-user
+    nad_lavender-eng \
+    nad_lavender-userdebug \
+    nad_lavender-user
--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
 	 $(DEVICE_PATH)/overlay \
-	 $(DEVICE_PATH)/overlay-lineage
+	 $(DEVICE_PATH)/overlay-nad
 
 # Audio
 PRODUCT_COPY_FILES += \
