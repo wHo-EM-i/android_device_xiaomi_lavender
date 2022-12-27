1afd85cdc57154abd1a53ae500cef5060fc5e053
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-12-27 12:09:58 +0530

    lavender: crDroid bringup
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_lavender-eng \
-    revengeos_lavender-userdebug \
-    revengeos_lavender-user
+    lineage_lavender-eng \
+    lineage_lavender-userdebug \
+    lineage_lavender-user
