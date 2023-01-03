487dc8f3147645569662e816464b3588bda92bbb
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-01-03 23:55:01 +0530

    lavender: StagOS bringup
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_lavender.mk
+    $(LOCAL_DIR)/stag_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_lavender-eng \
-    revengeos_lavender-userdebug \
-    revengeos_lavender-user
+    stag_lavender-eng \
+    stag_lavender-userdebug \
+    stag_lavender-user
